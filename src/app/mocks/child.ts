83b38c8292921eabb04dd797cd1f846344f25251
junaanjuna <yellowbyme0503@gmail.com>
2026-03-26 15:16:35 +0900

    feat : implement mypage UI and interaction

--- src/app/mocks/child.ts
+++ src/app/mocks/child.ts
@@ -2,29 +2,43 @@ export const mockChildren = [
   {
     id: 1,
     name: "아이 1",
+    birthdate: "2021-05-03",
+    gender: "F",
+    height: 102,
+    weight: 16,
+    allergies: "우유",
+    medicalHistory: "비염",
   },
   {
     id: 2,
     name: "아이 2",
+    birthdate: "2023-01-10",
+    gender: "M",
+    height: 89,
+    weight: 12,
+    allergies: "",
+    medicalHistory: "",
   },
+
   {
     id: 3,
     name: "아이 3",
+    birthdate: "2023-01-11",
+    gender: "F",
+    height: 90,
+    weight: 14,
+    allergies: "egg",
+    medicalHistory: "",
   },
+
   {
     id: 4,
     name: "아이 4",
-  },
-  {
-    id: 5,
-    name: "아이 5",
-  },
-  {
-    id: 6,
-    name: "아이 6",
-  },
-  {
-    id: 7,
-    name: "아이 7",
+    birthdate: "2023-09-10",
+    gender: "M",
+    height: 100,
+    weight: 20,
+    allergies: "milk",
+    medicalHistory: "해열제",
   },
 ];
